7e99e285879776e8ded3fe5708f990299142cc4b
johnpatrick23 <j-patrick@cloudswyft.com>
2019-11-05 18:13:46 +0800

    bvt, id_rsa

--- config/stamp/default/bvt.sh
+++ config/stamp/default/bvt.sh
@@ -17,8 +17,8 @@ ENVIRONMENT=bvt
 
 # the site certificate files (.crt & .key) have to be named after the BASE_URL value
 BASE_URL=cloudswyft.com 
-LMS_URL=oxatest-learning.$BASE_URL
-CMS_URL=oxatest-studio.$BASE_URL
+LMS_URL=oxarepublic-learning.$BASE_URL
+CMS_URL=oxarepublic-studio.$BASE_URL
 PREVIEW_URL=preview.$BASE_URL
 
 # deployment configuration
